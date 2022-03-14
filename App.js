3d9868931cb2af26e47fde21bf8a9a99de3e242d
embers <935267851@qq.com>
2022-03-15 00:38:51 +0800

    权限弹窗（未完成）

--- App.js
+++ App.js
@@ -12,45 +12,13 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
   View,
+  PermissionsAndroid,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+import {useEffect} from 'react/cjs/react.development';
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -58,6 +26,23 @@ const App: () => Node = () => {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+  const requestPermissions = async () => {
+    try {
+      const permissions = [PermissionsAndroid.PERMISSIONS.CAMERA];
+      const granted = await PermissionsAndroid.request(permissions, {
+        title: '我要相机权限',
+        message: '没权限搞毛给我开',
+      });
+      console.log(granted);
+      return granted;
+    } catch (err) {
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    requestPermissions();
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -66,47 +51,10 @@ const App: () => Node = () => {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        <View />
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
